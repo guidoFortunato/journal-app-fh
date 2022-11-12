a2c5073bb6e9a204e88fad2bb5e59fe9d94320fb
Guido <46787485+guidoFortunato@users.noreply.github.com>
2022-11-12 20:23:54 -0300

    activo una nota al darle click en el side bar

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,22 +1,18 @@
-import { TurnedInNot } from "@mui/icons-material";
 import { useSelector } from "react-redux";
 import {
   Box,
   Divider,
   Drawer,
-  Grid,
   List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { SideBarItem } from "./";
 
 export const SideBar = ({ drawerWidth }) => {
-  const {displayName} = useSelector(state => state.auth)
-  
+  const { displayName } = useSelector((state) => state.auth);
+  const { notes } = useSelector((state) => state.journal);
+
   return (
     <Box
       component="nav"
@@ -41,22 +37,8 @@ export const SideBar = ({ drawerWidth }) => {
         <Divider />
 
         <List>
-          {["Enero", "Febrero", "Marzo", "Abril"].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TurnedInNot />
-                </ListItemIcon>
-                <Grid container>
-                  <ListItemText primary={text} />
-                  <ListItemText
-                    secondary={
-                      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, distinctio! "
-                    }
-                  />
-                </Grid>
-              </ListItemButton>
-            </ListItem>
+          {notes.map((note) => (
+           <SideBarItem key={ note.id } { ...note } />
           ))}
         </List>
       </Drawer>
---
+++ src/journal/components/SideBarItem.jsx
@@ -0,0 +1,37 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+  Grid,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal";
+
+export const SideBarItem = ({ title = "", body, id, date, imageURL = [] }) => {
+  const newTitle = useMemo(() => {
+    return title.length > 15 ? title.substring(0, 15) + "..." : title;
+  }, [title]);
+
+  const dispatch = useDispatch();
+
+  const onClickNote = () => {
+    dispatch(setActiveNote({ title, body, id, date, imageURL }));
+  };
+
+  return (
+    <ListItem disablePadding>
+      <ListItemButton onClick={onClickNote}>
+        <ListItemIcon>
+          <TurnedInNot />
+        </ListItemIcon>
+        <Grid container>
+          <ListItemText primary={newTitle} />
+          <ListItemText secondary={body} />
+        </Grid>
+      </ListItemButton>
+    </ListItem>
+  );
+};
